441b4b32b98cbc869ff4b1be38dcc77d35bad9cb
cubewhy <cubewhy@lunarcn.lol>
2023-10-28 11:10:08 +0800

    INIT COMMIT

---
+++ src/main/java/top/lunarclient/ModEntry.java
@@ -0,0 +1,33 @@
+package top.lunarclient;
+
+import net.minecraft.crash.CrashReport;
+import org.cubewhy.lunarcn.loader.api.ModInitializer;
+import org.jetbrains.annotations.NotNull;
+
+public class ModEntry implements ModInitializer {
+
+    @Override
+    public void onCrash(@NotNull CrashReport crashReport) {
+
+    }
+
+    @Override
+    public void onInit() {
+
+    }
+
+    @Override
+    public void onPreInit() {
+
+    }
+
+    @Override
+    public void onStart() {
+
+    }
+
+    @Override
+    public void onStop() {
+
+    }
+}
---
+++ src/main/java/top/lunarclient/mixins/MixinGuiConnecting.java
@@ -0,0 +1,34 @@
+package top.lunarclient.mixins;
+
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.multiplayer.GuiConnecting;
+import net.minecraft.client.multiplayer.ServerData;
+import org.cubewhy.lunarcn.utils.RenderUtils;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(GuiConnecting.class)
+public abstract class MixinGuiConnecting extends GuiScreen {
+    /**
+     * @author cubewhy
+     * @reason liquid loading
+     */
+    @Overwrite
+    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+        this.drawDefaultBackground();
+
+        RenderUtils.drawLoadingCircle(this.width / 2, this.height / 4 + 70);
+
+        String ip = "Unknown";
+
+        final ServerData serverData = mc.getCurrentServerData();
+        if(serverData != null)
+            ip = serverData.serverIP;
+
+        this.drawCenteredString(mc.fontRendererObj, "Connecting to", this.width / 2, this.height / 4 + 110, 0xFFFFFF);
+        this.drawCenteredString(mc.fontRendererObj, ip, this.width / 2, this.height / 4 + 120, 0x5281FB);
+
+        super.drawScreen(mouseX, mouseY, partialTicks);
+    }
+
+}
